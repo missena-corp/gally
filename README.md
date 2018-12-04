58cdd3e7bcee8dac2a2dc42323030d61e535ea12
Jean-Sébastien Ney <jeansebastien.ney@gmail.com>
2018-12-04 16:11:43 +0100

    some real tests

--- README.md
+++ README.md
@@ -48,3 +48,4 @@ On `master` branch test the updated projects the previous commit
 - [ ] Find a way to avoid running multiple time tests for project sharing the same tests
 - [ ] Handle builds
 - [ ] Automatically generate releases
+- [ ] Ability to ignore pattern
--- project/project_test.go
+++ project/project_test.go
@@ -1,1 +1,21 @@
 package project
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestFindProjects(t *testing.T) {
+	t.Parallel()
+	projects, err := FindProjects("..")
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+	expected := []string{"../_examples"}
+	if !cmp.Equal(projects, expected) {
+		t.Error("projects", projects, "must be equal to", expected)
+		t.FailNow()
+	}
+}
