22ab425c92e55e891a290616e3182b1552d0b0bf
Jean-Sébastien Ney <jeansebastien.ney@gmail.com>
2018-12-04 16:27:52 +0100

    some test on Run

--- project/project_test.go
+++ project/project_test.go
@@ -15,7 +15,22 @@ func TestFindProjects(t *testing.T) {
 	}
 	expected := []string{"../_examples"}
 	if !cmp.Equal(projects, expected) {
-		t.Error("projects", projects, "must be equal to", expected)
+		t.Errorf("projects %v must be equal to %v", projects, expected)
+		t.FailNow()
+	}
+}
+
+func TestRun(t *testing.T) {
+	t.Parallel()
+	c := Config{Scripts: map[string]string{"hello": "echo world"}}
+	out, err := c.Run("hello")
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+	expected := []byte("world\n")
+	if !cmp.Equal(out, expected) {
+		t.Errorf("output must be equal to %q but is equal to %q", expected, out)
 		t.FailNow()
 	}
 }
