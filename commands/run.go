f7d2b3eb5e763b4dd7420d2af9d2b310c77196ae
Jean-Sébastien Ney <jeansebastien.ney@gmail.com>
2018-12-04 09:43:27 +0100

    rename config to project

--- commands/run.go
+++ commands/run.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"os/exec"
 
-	"github.com/missena-corp/gally/config"
+	"github.com/missena-corp/gally/project"
 	"github.com/spf13/cobra"
 	jww "github.com/spf13/jwalterweatherman"
 )
@@ -13,17 +13,18 @@ var runCmd = &cobra.Command{
 	Use:   "run",
 	Short: "let Gally run your script for updated files",
 	Run: func(cmd *cobra.Command, args []string) {
-		configs := config.UpdatedProjectConfig()
+		configs := project.UpdatedProjectConfig()
+		fmt.Printf("Gally monorepo handler %v", configs)
 		for _, c := range configs {
 			script, ok := c.Scripts[args[0]]
 			if !ok {
 				jww.ERROR.Printf("script %s not available", args[0])
+				continue
 			}
 			if _, err := exec.Command(script).Output(); err != nil {
 				jww.ERROR.Fatalf("could not run properly script %s: %v", script, err)
 			}
 		}
-		fmt.Printf("Gally monorepo handler %v", configs)
 	},
 }
 
