425c819b2b70f9fa87ca25af4f61f55c3892c990
Jean-Sébastien Ney <jeansebastien.ney@gmail.com>
2018-12-06 12:22:42 +0100

    List projects

---
+++ commands/list.go
@@ -0,0 +1,24 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/missena-corp/gally/project"
+	"github.com/spf13/cobra"
+)
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "display all handled projects",
+	Run: func(cmd *cobra.Command, args []string) {
+		handleVerboseFlag()
+		projects := project.FindProjects()
+		for _, p := range projects {
+			fmt.Printf("* %s: %s", p.Name, p.Dir)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+}
