e8c57bbe790272b9b2830fd4378f8dd0296c07a3
Jean-Sébastien Ney <jeansebastien.ney@gmail.com>
2018-12-20 18:10:58 +0100

    list now return a JSON

--- commands/list.go
+++ commands/list.go
@@ -1,9 +1,8 @@
 package commands
 
 import (
+	"encoding/json"
 	"fmt"
-	"os"
-	"text/tabwriter"
 
 	"github.com/missena-corp/gally/project"
 	"github.com/spf13/cobra"
@@ -16,12 +15,8 @@ var listCmd = &cobra.Command{
 	Short: "display all handled projects",
 	Run: func(cmd *cobra.Command, args []string) {
 		handleVerboseFlag()
-		w := tabwriter.NewWriter(os.Stdout, 0, 0, padding, ' ', tabwriter.Debug)
-		fmt.Fprintf(w, "Name\t Directory\t\n")
-		for _, p := range project.FindAll(rootDir) {
-			fmt.Fprintf(w, "%s\t %s\t\n", p.Name, p.BaseDir)
-		}
-		w.Flush()
+		out, _ := json.MarshalIndent(project.FindAll(rootDir).ToSlice(), "", "\t")
+		fmt.Println(string(out))
 	},
 }
 
